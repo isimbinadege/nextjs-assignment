efe1bd1581f3ad7aff19a92ac6e70ccfd5d86c12
isimbinadege <isimbi0123@gmail.com>
2025-11-07 05:30:40 +0200

    done with homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
 
+import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function HomePage() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-      <h1 className="bg-red-500">GOOD MORNING</h1> 
+    <div className="text-center mt-10 ">
+      <h1 className="text-4xl font-bold mb-4">Welcome to MyApp 🎉</h1>
+      <p className="text-lg">Current Time </p>
+      <p className="text-2xl font-semibold mt-2">{time.toLocaleTimeString()}</p>
     </div>
   );
 }
